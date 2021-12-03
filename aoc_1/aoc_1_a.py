86745c47cb9ce45a59caeb201c5ec3451dbbda34
Ernie White <erniewhite@embarqmail.com>
2021-12-03 05:20:29 -0600

    Moving code to repo

---
+++ aoc_1/aoc_1_a.py
@@ -0,0 +1,16 @@
+def main():
+    with open('./input.txt', 'r', newline='') as file:
+        sonar_readings = file.readlines();
+
+    previous_depth = int(sonar_readings[0]);
+    increase = 0;
+    for sonar_reading in sonar_readings[1:]:
+        current_depth = int(sonar_reading)
+        if current_depth and previous_depth < current_depth:
+            increase += 1
+        previous_depth = current_depth
+    print(increase)
+
+
+if __name__ == '__main__':
+    main()
