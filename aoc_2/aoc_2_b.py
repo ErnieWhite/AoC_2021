ca917e5d2968e4fa0644e3261e3a03aa63342e28
Ernie White <erniewhite@embarqmail.com>
2021-12-03 06:20:05 -0600

    Working Solution

--- aoc_2/aoc_2_b.py
+++ aoc_2/aoc_2_b.py
@@ -1,18 +1,28 @@
 def main():
-    with open('submove.txt') as file:
+    with open('test_input.txt') as file:
         directions = file.readlines()
     horizontal = 0
     depth = 0
-    sub_moves = {}
+    sub_info = {
+        'forward': 0,
+        'depth': 0,
+        'aim': 0,
+    }
     for direction in directions:
         command, amount = direction.split()
-        sub_moves[command] = sub_moves.get(command, 0) + int(amount)
+        match command:
+            case 'forward':
+                sub_info['forward'] += int(amount)
+                sub_info['depth'] += sub_info['aim'] * int(amount)
+            case 'down':
+                sub_info['aim'] += int(amount)
+            case 'up':
+                sub_info['aim'] -= int(amount)
         print(f'Move {command} {amount} times.')
-    horizontal = sub_moves['forward']
-    depth = sub_moves['down'] - sub_moves['up']
-    print(f'Horriontal: {horizontal}')
-    print(f'Depth: {depth}')
-    print(f'Product: {horizontal*depth}')
+    print(f'Forward: {sub_info["forward"]}')
+    print(f'Depth: {sub_info["depth"]}')
+    print(f'Aim: {sub_info["aim"]}')
+    print(f'Product: {sub_info["forward"] * sub_info["depth"]}')
 
 
 if __name__ == '__main__':
