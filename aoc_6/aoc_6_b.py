d09afd9489c586e0ccaf7242dbc031e0830fa5ad
Ernie White <erniewhite@embarqmail.com>
2021-12-06 06:28:36 -0600

    Change from dict to list

--- aoc_6/aoc_6_b.py
+++ aoc_6/aoc_6_b.py
@@ -1,7 +1,7 @@
 def main():
     with open('test_input.txt', 'r') as lanternfish_file:
         starting_fishes = [int(x) for x in lanternfish_file.read().split(',')]
-        fishes = {x: 0 for x in range(9)}
+        fishes = [0] * 9
         for fish in starting_fishes:
             fishes[fish] += 1
         for day in range(256):
@@ -10,7 +10,7 @@ def main():
                 fishes[timer] = fishes[timer + 1]
             fishes[8] = baby_fish
             fishes[6] += baby_fish
-        print(sum(fishes.values()))
+        print(sum(fishes))
 
 
 if __name__ == '__main__':
